223bce1c474385143aaa032d39987ed5b56a8f26
ranga c y <rajyc6991@gmail.com>
2021-06-03 10:17:17 +0530

    products/product page done

--- front-end-work/src/App.js
+++ front-end-work/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screen/HomeScreen';
+import ProductScreen from './screen/ProductScreen';
+import { Container } from 'react-bootstrap';
 const App = () => {
   return (
-    <div>
-      <h1>hello world</h1>
-    </div>
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Route path='/' exact component={HomeScreen}></Route>
+          <Route path='/product/:id' exact component={ProductScreen}></Route>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 };
 
