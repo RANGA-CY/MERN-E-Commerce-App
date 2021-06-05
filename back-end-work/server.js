520feb63740b7896732f7df5aa4be4c656769df5
ranga c y <rajyc6991@gmail.com>
2021-06-05 13:52:59 +0530

    done with the athentication

--- back-end-work/server.js
+++ back-end-work/server.js
@@ -5,10 +5,11 @@ import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import colors from 'colors';
 import productRoutes from './routes/productRoutes.js';
-
+import userRoutes from './routes/userRoutes.js';
 dotenv.config();
 connectDB();
 const app = express();
+app.use(express.json());
 // app.use((req, res, next) => {
 //   console.log(req.originalUrl);
 //   next();
@@ -18,6 +19,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
