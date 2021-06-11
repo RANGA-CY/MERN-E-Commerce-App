16c97896ba230b599ad8cc55a1f74b84f2c20e48
ranga c y <rajyc6991@gmail.com>
2021-06-11 09:47:41 +0530

    prepared for deployment

--- back-end-work/server.js
+++ back-end-work/server.js
@@ -25,10 +25,6 @@ app.use(express.json())
 //   next();
 // });
 
-app.get('/', (req, res) => {
-  res.status(200).send('hello people')
-})
-
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
@@ -38,6 +34,19 @@ app.get('/api/config/paypal', (req, res) => {
 })
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/front-end-work/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, 'front-end-work', 'build', 'index.html')
+    )
+  })
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
+
 app.use(notFound)
 app.use(errorHandler)
 
